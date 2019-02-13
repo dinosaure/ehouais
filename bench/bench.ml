3c02fce2e3507b5913b9369162cc4b54680d9b3a
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-02-13 16:50:14 +0100

    Add iter function

--- bench/bench.ml
+++ bench/bench.ml
@@ -26,6 +26,8 @@ let tbl =
 open Core
 open Core_bench
 
+module Set = Hashset.Make(struct type t = int let equal a b = (compare : int -> int -> int) a b = 0 let hash x = x end)
+
 let add_in_ewah tbl =
   let ewah = Ewah.make ~allocator:Ewah.allocator in
   let pos = ref 0 in
@@ -35,8 +37,6 @@ let add_in_ewah tbl =
          | true -> ignore @@ Ewah.add ewah !pos ; incr pos
          | false -> incr pos)
       tbl
-
-module Set = Hashset.Make(struct type t = int let equal a b = (compare : int -> int -> int) a b = 0 let hash x = x end)
 
 let add_in_hashset tbl =
   let set = Set.create 32 (* XXX(dinosaure): [ewah] starts with [32]. *) in
@@ -56,13 +56,33 @@ let add_once_in_hashset =
   let set = Set.create 32 in
   fun () -> Set.add set 1
 
+let iter_and_do_nothing_on_ewah =
+  let ewah = Ewah.make ~allocator:Ewah.allocator in
+  Stdlib.Hashtbl.iter (fun _ -> function
+      | true -> ignore @@ Ewah.add ewah !pos ; incr pos
+      | false -> incr pos)
+    tbl ;
+  fun () -> Ewah.each_bit ewah (fun _ () -> ()) ()
+
+let iter_and_do_nothing_on_hashset =
+  let set = Hashset.create 32 in
+  Stdlib.Hashtbl.iter
+    (fun _ -> function
+       | true -> Set.add set !pos ; incr pos
+       | false -> incr pos)
+    tbl ;
+  fun () -> Set.iter (fun _ -> ()) set
+
 let test_add_once_in_ewah = Test.create ~name:"ewah.add_once" add_once_in_ewah
 let test_add_once_in_hashset = Test.create ~name:"set.add" add_once_in_hashset
 let test_add_in_ewah = Test.create ~name:"ewah.add" (add_in_ewah tbl)
 let test_add_in_hashset = Test.create ~name:"set.add" (add_in_hashset tbl)
+let test_iter_on_ewah = Test.create ~name:"ewah.iter" iter_and_do_nothing_on_ewah
+let test_iter_on_hashset = Test.create ~name:"set.iter" iter_and_do_nothing_on_hashset
 
 let command =
   Bench.make_command [ test_add_in_ewah; test_add_in_hashset
-                     ; test_add_once_in_ewah; test_add_once_in_hashset ]
+                     ; test_add_once_in_ewah; test_add_once_in_hashset
+                     ; test_iter_on_ewah; test_iter_on_hashset ]
 
 let () = Command.run command
